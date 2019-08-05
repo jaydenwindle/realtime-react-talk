396e8e85f5a8b6b145c140ae9efdd4caebc91df5
Jayden Windle <jaydenwindle@gmail.com>
2019-08-05 15:43:59 -0700

    polling example

--- example_app/src/App.js
+++ example_app/src/App.js
@@ -1,19 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import daytime from "./daytime.png";
 import nighttime from "./nighttime.png";
+
 import "./App.css";
+
+const API_URL = "http://localhost:4000/polling";
 
 function App() {
   const [isDayTime, setIsDayTime] = useState(true);
+  const backgroundColor = isDayTime ? "#efefef" : "#212121";
+
+  useEffect(() => {
+    setInterval(async () => {
+      const response = await (await fetch(API_URL)).text();
+      setIsDayTime(response === "true");
+    }, 500);
+  }, []);
 
   return (
-    <div
-      className="App"
-      style={{ backgroundColor: isDayTime ? "#efefef" : "#212121" }}
-    >
+    <div className="App" style={{ backgroundColor }}>
       <img
         src={isDayTime ? daytime : nighttime}
-        onClick={() => setIsDayTime(!isDayTime)}
+        onClick={() => fetch(API_URL, { method: "POST" })}
         alt={isDayTime ? "Daytime" : "Night time"}
       />
     </div>
